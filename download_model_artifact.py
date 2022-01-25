dd480385c17b8c1ca5029353807e66a3fdb3cf7e
Ayush Thakur <ayushthakur@Ayushs-MacBook-Pro.local>
2022-01-25 20:24:56 +0530

    update

--- download_model_artifact.py
+++ download_model_artifact.py
@@ -1,13 +1,24 @@
 import wandb
-from argparse
+import argparse
 
 def get_args_parser():
-  parser = argparse.ArgumentParser('Download model saved as W&B Artifact and upload as Kaggle Dataset', add_help=False)
-  parser.add_argument('--artifact_name', default=64, type=int,
-                        help='Per GPU batch size')
+    parser = argparse.ArgumentParser('Download model saved as W&B Artifact and upload as Kaggle Dataset', add_help=False)
+    parser.add_argument('--artifact_name', type=str,
+                        help='to do')
+    parser.add_argument('--project_name', type=str, default=None,
+                        help='Name of the W&B project')
+    return parser
 
+def main(args):
+    run = wandb.init(project=args.project_name)
+    artifact = run.use_artifact(f'{args.artifact_name}:latest', type='model')
+    artifact_dir = artifact.download()
+    print(artifact_dir)
 
-run = wandb.init()
-artifact = run.use_artifact('ayut/sr_ffhq/3jygvqe3_model:v26', type='model')
-artifact_dir = artifact.download()
-print(artifact_dir)
+    return artifact_dir
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('Model download and upload script', parents=[get_args_parser()])
+    args = parser.parse_args()
+    
+    artifact_dir = main(args)
