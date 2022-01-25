8264eb22c1ef2b70a0ea92bcea2679646a3e40c9
Ayush Thakur <31141479+ayulockin@users.noreply.github.com>
2022-01-25 15:35:12 +0530

    Update and rename check.py to download_model_artifact.py

---
+++ download_model_artifact.py
@@ -0,0 +1,7 @@
+import wandb
+print("Using W&B version: ", wandb.__version__)
+
+run = wandb.init(anonymous='allow')
+artifact = run.use_artifact('ayut/sr_ffhq/3jygvqe3_model:v26', type='model')
+artifact_dir = artifact.download()
+print(artifact_dir)
