788a2d4dca31baccb5552132f6dfae20673ffa6e
Ayush Thakur <31141479+ayulockin@users.noreply.github.com>
2022-01-25 16:12:36 +0530

    Update download_model_artifact.py

--- download_model_artifact.py
+++ download_model_artifact.py
@@ -1,9 +1,7 @@
 import wandb
 print("Using W&B version: ", wandb.__version__)
 
-wandb.login(anonymously=True)
-
-run = wandb.init(anonymous='allow')
+run = wandb.init()
 artifact = run.use_artifact('ayut/sr_ffhq/3jygvqe3_model:v26', type='model')
 artifact_dir = artifact.download()
 print(artifact_dir)
