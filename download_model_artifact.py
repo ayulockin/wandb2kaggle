104406e83aa511905dca30a9df4ee6b468be8117
Ayush Thakur <31141479+ayulockin@users.noreply.github.com>
2022-01-25 19:50:37 +0530

    Update download_model_artifact.py

--- download_model_artifact.py
+++ download_model_artifact.py
@@ -1,5 +1,11 @@
 import wandb
-print("Using W&B version: ", wandb.__version__)
+from argparse
+
+def get_args_parser():
+  parser = argparse.ArgumentParser('Download model saved as W&B Artifact and upload as Kaggle Dataset', add_help=False)
+  parser.add_argument('--artifact_name', default=64, type=int,
+                        help='Per GPU batch size')
+
 
 run = wandb.init()
 artifact = run.use_artifact('ayut/sr_ffhq/3jygvqe3_model:v26', type='model')
